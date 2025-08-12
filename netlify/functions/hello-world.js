6baed7807372bd76c81c403663da04d00373adda
Edward Banner <edward@Edwards-Air.localdomain>
2025-08-12 13:24:40 -0400

    Start a call

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,6 +2,10 @@ const { WebClient } = require("@slack/web-api");
 
 const SLACK_BOT_TOKEN = process.env.EDWARDS_SLACKBOT_DEV_SLACK_BOT_TOKEN;
 
+const ZOOM_JOIN_URL = process.env.ZOOM_JOIN_URL;
+
+const ZOOM_DESKTOP_APP_JOIN_URL = process.env.ZOOM_DESKTOP_APP_JOIN_URL;
+
 const slack = new WebClient(SLACK_BOT_TOKEN);
 
 
@@ -17,8 +21,22 @@ exports.handler = async function(event) {
     text: `Hello from Netlify! You ran ${command}`
   });
 
+  // Create a Slack call and post a call block
+  const created = await slack.calls.add({
+    title: "co-working-room",
+    external_unique_id: "0xDEADBEEF",
+    join_url: ZOOM_JOIN_URL,
+    desktop_app_join_url: ZOOM_DESKTOP_APP_JOIN_URL,
+  });
+
+  const call_id = created?.call?.id;
+  await slack.chat.postMessage({
+    channel: 'co-working-room',
+    blocks: [{ type: "call", call_id }],
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ received: command}),
+    body: JSON.stringify({ received: call_id}),
   };
 };
