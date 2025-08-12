5ceebfd6bbe7f0a4df53d9559d94ff09616820b4
Edward Banner <edward@Edwards-Air.localdomain>
2025-08-12 13:07:57 -0400

    Return command

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,6 +1,12 @@
-exports.handler = async function(event, context) {
+exports.handler = async function(event) {
+  // console.log('event', event)
+
+  const params = new URLSearchParams(event.body);
+  const command = params.get("command");
+  console.log("Slash command:", command);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, World!" }),
+    body: JSON.stringify({ received: command}),
   };
 };
