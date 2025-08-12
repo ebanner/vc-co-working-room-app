3b99f131d62e189b69614c8fb66b3755850688ba
Edward Banner <edward@Edwards-Air.localdomain>
2025-08-12 13:18:24 -0400

    Send a slack message

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,9 +1,21 @@
+const { WebClient } = require("@slack/web-api");
+
+const SLACK_BOT_TOKEN = process.env.EDWARDS_SLACKBOT_DEV_SLACK_BOT_TOKEN;
+
+const slack = new WebClient(SLACK_BOT_TOKEN);
+
+
 exports.handler = async function(event) {
   // console.log('event', event)
 
   const params = new URLSearchParams(event.body);
   const command = params.get("command");
   console.log("Slash command:", command);
+
+  await slack.chat.postMessage({
+    channel: "co-working-room",
+    text: `Hello from Netlify! You ran ${command}`
+  });
 
   return {
     statusCode: 200,
